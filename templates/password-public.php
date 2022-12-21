aed8b1ca0cf8c1012049aac12eaef380a83aeb51
John Molakvoæ <skjnldsv@protonmail.com>
2022-12-21 12:32:41 +0100

    Update apps design and 25 compatibility
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- templates/password-public.php
+++ templates/password-public.php
@@ -23,28 +23,41 @@ declare(strict_types=1);
  */
 
 script('preferred_providers', 'password-public');
+style('preferred_providers', 'password-public');
 
 ?>
-<p class="info"><?php p($l->t('After this step, you will receive a mail to verify your account within 6 hours.')); ?></p>
-<form action="<?php print_unescaped($_['link']) ?>" id="set-password" method="post">
-	<fieldset>
-		<div class="grouptop">
-			<input type="hidden" value="<?php print_unescaped($_['ocsapirequest']) ?>" name="ocsapirequest">
-			<input type="email" name="email" id="email" value="<?php print_unescaped($_['email']) ?>" readonly />
-		</div>
-		<div class="groupbottom">
-			<input type="password" maxlength="100" name="password" id="password" value="" placeholder="<?php p($l->t('Set password')); ?>" required />
-			<input type="checkbox" id="show" name="show">
-			<label for="show"></label>
-		</div>
-		<div id="submit-wrapper">
-			<input class="login primary" type="submit" id="submit" value="<?php p($l->t('Log in')); ?>" />
-			<div class="submit-icon icon-confirm-white"></div>
-		</div>
-	</fieldset>
-	<?php if ($_['error']) { ?>
-		<p class="warning error">
-		   <?php print_unescaped($_['error']) ?>
-		</p>
-	<?php } ?>
-</form>
+<div class="guest-box login-box">
+	<form action="<?php print_unescaped($_['link']) ?>" id="set-password" class="set-password-form" method="post">
+		<fieldset>
+			<!-- Title -->
+			<h2><?php p($l->t('Set your password')); ?></h2>
+
+			<!-- Email -->
+			<div>
+				<label for="email"><?php p($l->t('Account name or email')); ?></label>
+				<input type="email" name="email" id="email" value="<?php print_unescaped($_['email']) ?>" readonly />
+			</div>
+
+			<!-- Password -->
+			<div>
+				<label for="password"><?php p($l->t('Password')); ?></label>
+				<input type="password" maxlength="100" name="password" id="password" value="" placeholder="<?php p($l->t('Set password')); ?>" required />
+				<input type="checkbox" id="show" name="show">
+				<label for="show"></label>
+			</div>
+
+			<!-- Submit -->
+			<div id="submit-wrapper">
+				<input type="hidden" value="<?php print_unescaped($_['ocsapirequest']) ?>" name="ocsapirequest">
+				<input class="login primary" type="submit" id="submit" value="<?php p($l->t('Log in')); ?>" />
+				<div class="submit-icon icon-confirm-white"></div>
+			</div>
+		</fieldset>
+		<p class="info"><?php p($l->t('After this step, you will receive a mail to verify your account within 6 hours.')); ?></p>
+		<?php if ($_['error']) { ?>
+			<p class="warning error">
+			<?php print_unescaped($_['error']) ?>
+			</p>
+		<?php } ?>
+	</form>
+</div>
